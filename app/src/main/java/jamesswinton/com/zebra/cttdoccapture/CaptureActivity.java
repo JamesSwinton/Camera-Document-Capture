c9b05583c9da28b72df08468ac9a9fb3df8c8168
JS6845 <JS6845@zebra.com>
2019-02-12 16:49:13 +0000

    Added Toolbar

--- app/src/main/java/jamesswinton/com/zebra/cttdoccapture/CaptureActivity.java
+++ app/src/main/java/jamesswinton/com/zebra/cttdoccapture/CaptureActivity.java
@@ -1,16 +1,32 @@
 package jamesswinton.com.zebra.cttdoccapture;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import jamesswinton.com.zebra.cttdoccapture.databinding.ActivityCaptureBinding;
 
 public class CaptureActivity extends AppCompatActivity {
 
     // Debugging
+    private static final String TAG = "CaptureActivity";
 
+    // Constants
+
+
+    // Variables
+    ActivityCaptureBinding mDataBinding = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_capture);
+
+        // Init DataBinding
+        mDataBinding = DataBindingUtil.setContentView(this, R.layout.activity_capture);
+
+        // Init Toolbar
+        setSupportActionBar(mDataBinding.toolbar);
+        getSupportActionBar().setTitle("CTT Document Capture");
+        getSupportActionBar().setSubtitle("Capture, process & store documents");
     }
 }
