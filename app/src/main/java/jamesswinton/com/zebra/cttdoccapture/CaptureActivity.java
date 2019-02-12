ce11afdadffd5ebe0b205f5ffd687df85ff5b968
JS6845 <JS6845@zebra.com>
2019-02-12 16:30:24 +0000

    Initial commit

---
+++ app/src/main/java/jamesswinton/com/zebra/cttdoccapture/CaptureActivity.java
@@ -0,0 +1,16 @@
+package jamesswinton.com.zebra.cttdoccapture;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CaptureActivity extends AppCompatActivity {
+
+    // Debugging
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_capture);
+    }
+}
