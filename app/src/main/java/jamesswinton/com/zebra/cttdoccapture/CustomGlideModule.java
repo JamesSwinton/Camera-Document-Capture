4729dc312acb496c5715ea8381963be313ed160c
JS6845 <JS6845@zebra.com>
2019-02-13 09:58:58 +0000

    Required empty class to generate GlideApp API

---
+++ app/src/main/java/jamesswinton/com/zebra/cttdoccapture/CustomGlideModule.java
@@ -0,0 +1,9 @@
+package jamesswinton.com.zebra.cttdoccapture;
+
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
+
+@GlideModule
+public class CustomGlideModule extends AppGlideModule {
+    // Required empty class to generate GlideApp API
+}
